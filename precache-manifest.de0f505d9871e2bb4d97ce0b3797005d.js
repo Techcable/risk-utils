2f831bc4c78da195d615f51b2643aec9ecb4b459
Techcable <Techcable@techcable.net>
2020-02-21 09:48:12 -0700

    Updates

---
+++ precache-manifest.de0f505d9871e2bb4d97ce0b3797005d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b101b8ca87387774e0e81859fdf9d92a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f9cb95af4a045448243",
+    "url": "/static/js/2.91c63949.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.91c63949.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f674df911b23b50d338f",
+    "url": "/static/js/main.118b199b.chunk.js"
+  },
+  {
+    "revision": "c758243d84c112071d79",
+    "url": "/static/js/runtime-main.6a460106.js"
+  }
+]);
